5b70a1f4562d83b58d14d6ce5334cba3e9d71e11
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-06-18 10:34:08 +0200

    better chat demo

--- test/chat-express.js
+++ test/chat-express.js
@@ -20,5 +20,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on http://localhost:3000/');
 });
--- test/chat-http.js
+++ test/chat-http.js
@@ -18,5 +18,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on http://localhost:3000/');
 });
